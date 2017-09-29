a819d866f8fab5fd827ebfd47a1ba48c4ae332a2
刘艳峰 <Walter0817@liuyanfengdeMacBook-Pro.local>
2017-09-29 18:22:15 +0800

    add HelloDemo1

---
+++ ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  Demo1
+//
+//  Created by 刘艳峰 on 2017/9/20.
+//  Copyright © 2017年 刘艳峰. All rights reserved.
+//
+
+
+for i:Int in 0..<10 {
+  print("ab202\(i)年")
+}
